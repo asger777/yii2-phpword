0446a1c5fca9521c4869aa77b507b0f58f8ed895
m0zart89 <mozart-freelance@yandex.ru>
2014-11-06 15:35:32 +0200

    10

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -2,6 +2,8 @@
 
 namespace m0zart89\phpword;
 
+use
+
 /**
  * This is just an example.
  */
@@ -9,6 +11,7 @@ class AutoloadExample extends \yii\base\Widget
 {
     public function run()
     {
+        $templateProcessor = new TemplateProcessor('Template.docx');
         return "Hello!";
     }
 }
